4232f08a1b9271778fc8e4c354edfb500edfaf2a
Damien Nelson <dhnisaman@gmail.com>
2023-03-19 14:17:08 -0400

    commit tree c3681a2cf7034424d34f7b4f1ea505f3b386d63c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_arr)
+    name_arr.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(arr)
+    arr.map.with_index {|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+end
+
+def printer(arr)
+    batch_badge_creator(arr).each {|el| puts el}
+    assign_rooms(arr).each {|el| puts el}
+end
